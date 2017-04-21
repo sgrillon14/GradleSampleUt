5394c737bdfcfe996d601b71524b131d5a370493
sgrillon <sgrillon@hotmail.fr>
2017-04-21 11:37:06 +0200

    init project

---
+++ src/test/java/com/github/sgrillon14/sampleut/AppTest.java
@@ -0,0 +1,17 @@
+package com.github.sgrillon14.sampleut;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testHelloWorld() {
+        App.main(null);
+        Assert.assertTrue(true);
+    }
+
+}
